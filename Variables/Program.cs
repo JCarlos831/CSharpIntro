0201155c27abddfe347fe5edabe8ed8ad6f1f373
Juan Carlos Montoya <jcmontoya@gmail.com>
2018-12-18 11:00:18 -0500

    Working with variables to create a number game

---
+++ Variables/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomNumber = new Random();
+            int firstNumber = randomNumber.Next(2, 10);
+            int secondNumber = randomNumber.Next(5, 10);
+
+            var subtraction = randomNumber.Next(7, 10);
+
+            int answer = firstNumber * secondNumber - subtraction;
+            string prompt = ". Don't type the answer, just press ENTER when ready...";
+
+            Console.WriteLine("Think of a number between 1 and 10" + prompt);
+            Console.ReadKey();
+
+            Console.WriteLine("Multiply your number by " + firstNumber + prompt);
+            Console.ReadKey();
+
+            Console.WriteLine("Now multiply your number by " + secondNumber + prompt);
+            Console.ReadKey();
+
+            Console.WriteLine("Divide the result by the number you originally though of" + prompt);
+            Console.ReadKey();
+
+            Console.WriteLine("Now subtract " + subtraction + prompt);
+            Console.ReadKey();
+
+            Console.WriteLine("The answer is " + answer);
+        }
+    }
+}
